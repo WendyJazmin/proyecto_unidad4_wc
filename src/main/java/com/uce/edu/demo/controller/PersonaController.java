17a368fc1d52fea76a481d403d91af310fd69425
wendyjazmin <wendyjazmin1994@hotmail.com>
2022-09-05 20:55:29 -0500

    taller38

--- src/main/java/com/uce/edu/demo/controller/PersonaController.java
+++ src/main/java/com/uce/edu/demo/controller/PersonaController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.uce.edu.demo.modelo.Persona;
@@ -23,6 +27,40 @@ public class PersonaController {
 	public String buscarTodos(Model modelo){
 		List<Persona> lista = this.personaService.buscarTodos();
 		modelo.addAttribute("personas",lista);
-		return "vistaListaEstudiantes";
+		return "vistaListaPersonas";
+	}
+	
+	@GetMapping("/buscarUno/{idPersona}")
+	public String buscarPersona(@PathVariable("idPersona")Integer id,Model modelo) {
+		System.out.println("EL ID: "+id);
+		Persona per = this.personaService.buscarPorId(id);
+		modelo.addAttribute("persona", per);
+		return "vistaPersona";
+	}
+	
+	@PutMapping("/actualizar/{idPersona}")
+	public String actualizarPersona(@PathVariable("idPersona")Integer id,Persona persona) {
+		persona.setId(id);
+		this.personaService.actualizar(persona);
+		return "redirect:/personas/buscar";
+	}
+	
+	
+	@DeleteMapping("/borrar/{idPersona}")
+	public String borrarPersona(@PathVariable("idPersona")Integer id) {
+		this.personaService.eliminar(id);
+		
+		return "redirect:/personas/buscar";
+	}
+	
+	@PostMapping("/insertar")
+	public String insertarPersona(Persona persona) {
+		this.personaService.guardar(persona);
+		return "redirect:/personas/buscar";
+	}
+	//Metodos de redireccionamiento a pagina
+	@GetMapping("/nuevaPersona")
+	public String paginaNuevaPersona(Persona persona) {
+		return "vistaNuevaPersona";
 	}
 }
